2a5b34ae28c9f6bc5c075b2b46a4ec443b6196df
cjislegit <carlosjramirez11@gmail.com>
2018-07-02 21:00:11 -0700

    Added second answer

--- Longest Word/longestWord.js
+++ Longest Word/longestWord.js
@@ -7,12 +7,22 @@ function longestWord(str){
         if(word.length < element.length){
             word = element;
         }
-    })
+    });
     
+    return word;
+}
+
+function longestWord2(str){
+    let filteredArray = str.replace(/[\W]/g, ' ').split(' ');
+    let word = '';
+
+    filteredArray.forEach((element) => {
+        word = element.length < word.length ? word : element;
+    });
     return word;
 }
 
 
 console.log(longestWord('Ready, set, go!'));
-console.log(longestWord('Ready[[[[, set, go!'));
+console.log(longestWord2('Ready[[[[, set, go!'));
 console.log(longestWord('ABc'));
